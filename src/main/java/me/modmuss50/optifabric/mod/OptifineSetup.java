09049dace7a6692052b71b41cb10a31002a7aa0b
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2024-09-17 16:26:49 +0800

    Update OptifineSetup.java

--- src/main/java/me/modmuss50/optifabric/mod/OptifineSetup.java
+++ src/main/java/me/modmuss50/optifabric/mod/OptifineSetup.java
@@ -19,7 +19,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
